3057a3124fc169e0563253ce9b5b6e00c0ca599c
abcijkxyz <abcijkxyz@163.com>
2025-02-21 17:06:34 +0800

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
+"use client"
+
 import { CardsDemo } from "@/components/cards"
 import { ThemePreview } from "@/components/theme-preview"
+import { useTheme } from "@/components/theme-provider"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { themeNames } from "@/themes"
 
 export default function Home() {
+  const { colorTheme, setColorTheme } = useTheme()
+  const theme = themeNames.find((theme) => theme.value === colorTheme)
   return (
     <Tabs defaultValue="theme" className="w-full shadow-xl bg-background">
       <TabsList className="grid w-full grid-cols-2">
         <TabsTrigger value="theme">Theme ({themeNames.length})</TabsTrigger>
-        <TabsTrigger value="preview">Preview</TabsTrigger>
+        <TabsTrigger value="preview">Preview ({theme?.name})</TabsTrigger>
       </TabsList>
       <TabsContent value="theme">
         <ThemePreview />
